6dcd6eb66338974aacd16dcc3249d7e673afd4f9
geovin89 <georgevincent8917@gmail.com>
2022-08-12 17:30:41 -0700

    Added HTTP Api endpoint to gateway

--- bagg_fresh_service/infra/bagg_fresh_service_stack.py
+++ bagg_fresh_service/infra/bagg_fresh_service_stack.py
@@ -1,10 +1,11 @@
 from aws_cdk import (
     aws_lambda as lmb,
-    aws_apigateway as apigw,
+    aws_apigatewayv2_alpha as apigw,
     CfnOutput,
     Stack,
 )
 from constructs import Construct
+from aws_cdk.aws_apigatewayv2_integrations_alpha import HttpLambdaIntegration
 from os import path
 from pathlib import Path
 
@@ -16,13 +17,19 @@ class BaggFreshServiceStack(Stack):
 
         root_dir = Path(path.dirname(__file__)).parent
 
-        handler = lmb.Function(self, 'Handler',
+        handler = lmb.Function(self, 'BaggFreshSellerLambda',
                                runtime=lmb.Runtime.PYTHON_3_8,
                                handler='simple_handler.handler',
                                code=lmb.Code.from_asset(path.join(root_dir, 'src/lambda')))
 
-        api_gateway = apigw.LambdaRestApi(self, 'Gateway',
-                                          description='Endpoint created for BaggFresh service',
-                                          handler=handler.current_version)
+        http_api = apigw.HttpApi(self, 'BaggFreshSellerHttpApi')
 
-        self.url_output = CfnOutput(self, 'Url', value=api_gateway.url)
+        http_api.add_routes(
+            path="/nearMe",
+            methods=[apigw.HttpMethod.GET],
+            integration=HttpLambdaIntegration(
+                'BaggFreshSellerLambdaIntegration', handler
+            )
+        )
+
+        self.url_output = CfnOutput(self, 'Url', value=http_api.url)
